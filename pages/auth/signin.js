2952d0e60877080910e6f2e7231e894dba3b1fa9
SalimAbdala27 <salimabdala27@hotmail.com>
2022-01-20 16:32:36 +0000

    re render

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -10,9 +10,8 @@ const signIn = (props) => {
       <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-44 px-14 text-center">
         <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
         <p className="font-xs italic"> The app is just a clone built for educational purposes.</p>
-        <h2>HELLO</h2>
         <div className="mt-40">
-          {providers && Object.values(providers).map((provider) => (
+          {Object.values(providers).map((provider) => (
             <div key={provider.name}>
               <button className="p-3 bg-blue-500 rounded-lg text-white"
                 onClick={() => SignIntoProvider(provider.id, {callbackUrl: "/"})}>
