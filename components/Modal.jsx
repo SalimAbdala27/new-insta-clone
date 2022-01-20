78667731644eb8a85a6460556d4095eec1a1c121
SalimAbdala27 <salimabdala27@hotmail.com>
2022-01-20 16:08:50 +0000

    modal swapped

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -88,11 +88,12 @@ const Modal = () => {
                   ref={captionRef}
                   placeholder="Please enter a capion..."/>
                 </div>
-                <div className="mt-5 sm:mt-6">
-                  <button type="button" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300" onClick={uploadPost}>
+                <div className="mt-5 sm:mt-6"> 
+                  <button type="button" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed" onClick={uploadPost}>
                     {loading ? "Uploading.." : "Upload Post"}
                   </button>
                 </div>
+                {/* hover:disabled:bg-gray-300 */}
               </div>
             </div>
           </Transition.Child>
