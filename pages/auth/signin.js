8adda5448352531d5374c95302a1ec897c9ef2ec
SalimAbdala27 <salimabdala27@hotmail.com>
2022-01-20 16:01:03 +0000

    finished insta

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -9,7 +9,7 @@ const signIn = (props) => {
       <Header />
       <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-44 px-14 text-center">
         <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
-        <p className="font-xs italic"> The app is just a clone built for educational purposes</p>
+        <p className="font-xs italic"> The app is just a clone built for educational purposes.</p>
         <div className="mt-40">
           {providers && Object.values(providers).map((provider) => (
             <div key={provider.name}>
